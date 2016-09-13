2cd5a4972a5e491af156a0ca98fc7ccd1d9f491f
Tom Kay <oridan82@gmail.com>
2016-09-13 11:08:15 +0100

    initial commit

---
+++ php-timecop-stubs.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Time travel to specified timestamp and freeze
+ * @param number $timestamp
+ */
+function timecop_freeze($timestamp = null) {}
+
+/**
+ * Time travel to specified timestamp
+ * @param number $timestamp
+ */
+function timecop_travel($timestamp = null) {}
+
+/**
+ * Return to unspecified timestamp
+ */
+function timecop_return() {}
