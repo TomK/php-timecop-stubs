c12bd62b5ea2bb8d10f8bde2dfc5b6ab701d1d58
Tom Kay <oridan82@gmail.com>
2016-09-13 11:51:08 +0100

    add return types

--- php-timecop-stubs.php
+++ php-timecop-stubs.php
@@ -3,16 +3,19 @@
 /**
  * Time travel to specified timestamp and freeze
  * @param int $timestamp
+ * @return bool
  */
 function timecop_freeze($timestamp) {}
 
 /**
  * Time travel to specified timestamp
  * @param int $timestamp
+ * @return bool
  */
 function timecop_travel($timestamp) {}
 
 /**
  * Return to unspecified timestamp
+ * @return bool
  */
 function timecop_return() {}
